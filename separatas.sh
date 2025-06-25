a446bbc954836d6d474c155c9b66aaeeff578934
J4vierVi <ja.vill4lba@gmail.com>
2025-06-25 00:35:43 -0300

    25-06-2025

--- separatas.sh
+++ separatas.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+pdftk ./pdf/pdf-20250501FARRAN.pdf cat 61-63 output ./docs/conclusiones-pdf-20250501FARRAN.pdf
+pdftk ./pdf/pdf-20250501FARRAN.pdf cat 64-72 output ./docs/indices-pdf-20250501FARRAN.pdf
+exit
