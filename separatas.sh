1e69075590152bdd7ffe5cc2f40c9295c8bdac11
J4vierVi <ja.vill4lba@gmail.com>
2025-06-29 02:15:10 -0300

    29-06-2025

--- separatas.sh
+++ separatas.sh
@@ -2,6 +2,7 @@
 pdftk ./pdf/pdf-20250501FARRAN.pdf cat 9-9 output ./docs/presentacion-pdf-20250501FARRAN.pdf
 pdftk ./pdf/pdf-20250501FARRAN.pdf cat 11-18 output ./docs/¿Por qué ontologías políticas-pdf-20250501FARRAN.pdf
 pdftk ./pdf/pdf-20250501FARRAN.pdf cat 19-59 output ./docs/Ontología de la diferencia-pdf-20250501FARRAN.pdf
+pdftk ./pdf/pdf-20250501FARRAN.pdf cat 63-64 output ./docs/Autores-pdf-20250501FARRAN.pdf
 pdftk ./pdf/pdf-20250501FARRAN.pdf cat 65-67 output ./docs/conclusiones-pdf-20250501FARRAN.pdf
 pdftk ./pdf/pdf-20250501FARRAN.pdf cat 68-78 output ./docs/indices-pdf-20250501FARRAN.pdf
 exit
